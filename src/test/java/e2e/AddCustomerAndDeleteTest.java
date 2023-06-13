85ae42f590586893dae0412e68fdf38cdfbf5bfd
LeonadO555 <lmikhailov@supplyshift.net>
2023-06-14 00:03:05 +0400

    Added new tests

--- src/test/java/e2e/AddCustomerAndDeleteTest.java
+++ src/test/java/e2e/AddCustomerAndDeleteTest.java
@@ -1,11 +1,16 @@
 package e2e;
 
 import com.github.javafaker.Faker;
+import enums.SortDirections;
+import enums.SortValues;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AddCustomerPage;
 import pages.CustomersPage;
 import pages.HomePage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddCustomerAndDeleteTest extends TestBase {
     Faker faker = new Faker();
@@ -18,6 +23,19 @@ public class AddCustomerAndDeleteTest extends TestBase {
         String firstName = faker.internet().uuid();
         String lastName = faker.internet().uuid();
         String postCode = faker.address().zipCode();
+        List<String> expectedListFirstNamesUp = new ArrayList<>();
+        expectedListFirstNamesUp.add("Ron");
+        expectedListFirstNamesUp.add("Neville");
+        expectedListFirstNamesUp.add("Hermoine");
+        expectedListFirstNamesUp.add("Harry");
+        expectedListFirstNamesUp.add("Albus");
+
+        List<String> expectedListFirstNamesDown = new ArrayList<>();
+        expectedListFirstNamesDown.add("Albus");
+        expectedListFirstNamesDown.add("Harry");
+        expectedListFirstNamesDown.add("Hermoine");
+        expectedListFirstNamesDown.add("Neville");
+        expectedListFirstNamesDown.add("Ron");
 
         homePage = new HomePage(app.driver);
         homePage.clickOnBankManagerLoginButton();
@@ -43,5 +61,32 @@ public class AddCustomerAndDeleteTest extends TestBase {
         Assert.assertEquals(customersPage.makeCellLocator(lastName).getText(), lastName, "Last name is not equals");
         Assert.assertEquals(customersPage.makeCellLocator(postCode).getText(), postCode, "Post code is not equals");
         Assert.assertEquals(customersPage.countRows(), 1, "Count rows is not equals should be 1 row");
+
+        customersPage.deleteTableRow(firstName);
+        customersPage.clearFilterCustomer();
+        customersPage.filterCustomer(firstName);
+        Assert.assertEquals(customersPage.countRows(), 0, "Count rows is not equals should be 0 row");
+
+        customersPage.clearFilterCustomer();
+        customersPage.clickOnSortLink(SortValues.fName);
+        customersPage.checkSortDirection(SortDirections.up);
+        List<String> actualListFirstNameColumnUp = customersPage.getTextFromCellFirstNameColumn();
+        Assert.assertEquals(actualListFirstNameColumnUp, expectedListFirstNamesUp, "Actual list with all cells column First name is not equals expected");
+
+        customersPage.clickOnSortLink(SortValues.fName);
+        customersPage.checkSortDirection(SortDirections.down);
+        List<String> actualListFirstNameColumnDown = customersPage.getTextFromCellFirstNameColumn();
+        Assert.assertEquals(actualListFirstNameColumnDown, expectedListFirstNamesDown, "Actual list with all cells column First name is not equals expected");
+
+        customersPage.clickOnSortLink(SortValues.lName);
+        customersPage.checkSortDirection(SortDirections.up);
+
+        customersPage.clickOnSortLink(SortValues.lName);
+        customersPage.checkSortDirection(SortDirections.down);
+
+        customersPage.clickOnSortLink(SortValues.postCd);
+        customersPage.checkSortDirection(SortDirections.up);
+        customersPage.clickOnSortLink(SortValues.postCd);
+        customersPage.checkSortDirection(SortDirections.down);
     }
 }
--- src/test/java/pages/CustomersPage.java
+++ src/test/java/pages/CustomersPage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomersPage extends BankManagerLoginPage {
     public CustomersPage(WebDriver driver) {
         super(driver);
@@ -24,11 +27,21 @@ public class CustomersPage extends BankManagerLoginPage {
     }
 
     public WebElement makeRowLocator(String name) {
-        return makeCellLocator(name).findElement(By.xpath("/ancestor::*[@class='ng-scope']"));
+        return driver.findElement(By.xpath("//*[@class='table table-bordered table-striped']//*[@class='ng-scope']//*[contains(text(), '" + name + "')]/ancestor::*[@ng-repeat='cust in Customers | orderBy:sortType:sortReverse | filter:searchCustomer']"));
     }
 
     public void clickOnSortLink(SortValues sortValue) {
         driver.findElement(By.xpath("//*[contains(@ng-click, \"sortType = '" + sortValue + "'\")]")).click();
+    }
+
+    public List<String> getTextFromCellFirstNameColumn() {
+        List<String> array = new ArrayList<>();
+        List<WebElement> cells = driver.findElements(By.xpath("//*[@ng-repeat='cust in Customers | orderBy:sortType:sortReverse | filter:searchCustomer']/td[1]"));
+        for (WebElement cell : cells) {
+            String text = cell.getText();
+            array.add(text);
+        }
+        return array;
     }
 
     public void checkSortDirection(SortDirections direction) {
@@ -39,8 +52,14 @@ public class CustomersPage extends BankManagerLoginPage {
         makeRowLocator(name).findElement(By.xpath("//*[@ng-click='deleteCust(cust)']")).click();
     }
 
+    //driver.findElement(By.xpath("//*[@ng-model='searchCustomer']")).clear() == searchCustomerField.clear()
+
 
     public void filterCustomer(String customer) {
         searchCustomerField.sendKeys(customer);
+    }
+
+    public void clearFilterCustomer() {
+        searchCustomerField.clear();
     }
 }
